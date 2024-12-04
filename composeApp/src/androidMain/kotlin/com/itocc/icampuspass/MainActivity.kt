06f8c961b766de9f28b98f91369f761bdc93341a
Winston Sung <winston21.sung@gmail.com>
2024-12-04 20:39:45 +0800

    Test UI and logic
    Change-Id: Iaa7f4c13307f66f835bb72532f4dc30be9acd62e

--- composeApp/src/androidMain/kotlin/com/itocc/icampuspass/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/itocc/icampuspass/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.itocc.icampuspass.App
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
+        setContent() {
             App()
         }
     }
