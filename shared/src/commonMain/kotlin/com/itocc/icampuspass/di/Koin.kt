15684590f5f9c804320cdac4eb2c3f01f1be7c9a
Winston Sung <winston21.sung@gmail.com>
2025-02-12 20:27:33 +0800

    chore: Add basic shared logic
    Bug: #21
    Change-Id: Id06acb17bbf6f66115428884231cd81e0a166983

--- shared/src/commonMain/kotlin/com/itocc/icampuspass/di/Koin.kt
+++ shared/src/commonMain/kotlin/com/itocc/icampuspass/di/Koin.kt
@@ -1,12 +1,41 @@
 package com.itocc.icampuspass.di
 
+import com.itocc.icampuspass.modals.AppRepository
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.cookies.HttpCookies
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-val dataModule = module {}
+val dataModule: Module = module {
+    single<AppRepository> {
+        AppRepository().apply {
+            init()
+        }
+    }
 
-fun initKoin() = initKoin(emptyList())
+    single<HttpClient> {
+        HttpClient {
+            install(plugin = ContentNegotiation) {
+                json(json = get(), contentType = ContentType.Any)
+            }
+
+            install(plugin = HttpCookies)
+        }
+    }
+
+    single<Json> {
+        Json {
+            ignoreUnknownKeys = true
+        }
+    }
+}
+
+fun initKoin() = initKoin(extraModules = emptyList())
 
 fun initKoin(extraModules: List<Module>) {
     startKoin {
---
+++ shared/src/commonMain/kotlin/com/itocc/icampuspass/viewmodals/MainViewModal.kt
@@ -0,0 +1,7 @@
+package com.itocc.icampuspass.viewmodals
+
+import com.itocc.icampuspass.modals.AppRepository
+import com.rickclephas.kmp.observableviewmodel.ViewModel
+
+class MainViewModal(repository: AppRepository): ViewModel() {
+}
