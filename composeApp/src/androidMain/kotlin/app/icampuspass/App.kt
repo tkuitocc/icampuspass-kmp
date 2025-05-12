3249bd03bf9f1155e0f87d7e0f08c40759781a48
Winston Sung <winston21.sung@gmail.com>
2025-05-12 14:04:41 +0800

    Test logic and Android UI
    Change-Id: Icd2bdd002b1d8c37a78c2ef5e203265e8634f11e

--- composeApp/src/androidMain/kotlin/app/icampuspass/App.kt
+++ composeApp/src/androidMain/kotlin/app/icampuspass/App.kt
@@ -2,23 +2,17 @@ package app.icampuspass
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Surface
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewDynamicColors
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
@@ -26,44 +20,94 @@ import androidx.compose.ui.tooling.preview.Wallpapers.BLUE_DOMINATED_EXAMPLE
 import androidx.compose.ui.tooling.preview.Wallpapers.GREEN_DOMINATED_EXAMPLE
 import androidx.compose.ui.tooling.preview.Wallpapers.RED_DOMINATED_EXAMPLE
 import androidx.compose.ui.tooling.preview.Wallpapers.YELLOW_DOMINATED_EXAMPLE
-import app.icampuspass.viewmodels.MainViewModel
+import androidx.lifecycle.compose.dropUnlessResumed
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import app.icampuspass.views.composables.ModalDrawerSheet
+import app.icampuspass.views.navigation.NavHost
+import app.icampuspass.views.navigation.destinations.AboutScreenDestination
+import app.icampuspass.views.navigation.destinations.MainScreenDestination
+import app.icampuspass.views.navigation.destinations.SettingsScreenDestination
 import app.icampuspass.views.theme.Theme
-import icampuspass.composeapp.generated.resources.Res
-import icampuspass.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
-import org.koin.androidx.compose.koinViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun App() {
     Theme {
-        val viewModel = koinViewModel<MainViewModel>()
+        Surface(color = Color.Black) {
+            Surface(modifier = Modifier.safeDrawingPadding()) {
+                val scope = rememberCoroutineScope()
 
-        var showContent by remember { mutableStateOf(value = false) }
+                val navController = rememberNavController()
 
-        Column(
-            modifier = Modifier
-                .background(color = MaterialTheme.colorScheme.surface)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text(text = "Click me!")
-            }
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
-            AnimatedVisibility(visible = showContent) {
-                val greeting = remember { Greeting().greet() }
+                ModalNavigationDrawer(
+                    drawerContent = {
+                        ModalDrawerSheet(
+                            navController = navController,
+                            onMenuClose = {
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            onNavigateMain = dropUnlessResumed {
+                                scope.launch {
+                                    drawerState.close()
+                                }
 
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        painter = painterResource(resource = Res.drawable.compose_multiplatform),
-                        contentDescription = null
-                    )
+                                scope.launch {
+                                    navController.navigate(route = MainScreenDestination) {
+                                        popUpTo(route = MainScreenDestination)
 
-                    Text(text = "Compose: $greeting")
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
+                                }
+                            },
+                            onNavigateSettings = dropUnlessResumed {
+                                scope.launch {
+                                    drawerState.close()
+                                }
+
+                                scope.launch {
+                                    navController.navigate(route = SettingsScreenDestination) {
+                                        popUpTo(route = MainScreenDestination)
+
+                                        launchSingleTop = true
+                                    }
+                                }
+                            },
+                            onNavigateAbout = dropUnlessResumed {
+                                scope.launch {
+                                    drawerState.close()
+                                }
+
+                                scope.launch {
+                                    navController.navigate(route = AboutScreenDestination) {
+                                        popUpTo(route = MainScreenDestination)
+
+                                        launchSingleTop = true
+                                    }
+                                }
+                            }
+                        )
+                    },
+                    drawerState = drawerState,
+                    content = {
+                        NavHost(
+                            scope = scope,
+                            navController = navController,
+                            drawerState = drawerState
+                        )
+                    }
+                )
+
+                BackHandler(enabled = drawerState.isOpen) {
+                    scope.launch {
+                        drawerState.close()
+                    }
                 }
             }
         }
