ea0066af6ccfffc2e2529a752baaf5185cf18c9a
Winston Sung <winston21.sung@gmail.com>
2025-05-12 14:04:41 +0800

    Test logic and Android UI
    Change-Id: Ifb669c26d587cefc32c4157ac1a086d4dd4e9245

---
+++ composeApp/src/androidMain/kotlin/com/itocc/icampuspass/composables/TopAppBar.kt
@@ -0,0 +1,66 @@
+package com.itocc.icampuspass.composables
+
+import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar() {
+    TopAppBar(
+        title = {
+            Text(text = "iCampusPass")
+        },
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = null
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Settings,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+@Preview(
+    showSystemUi = true,
+    backgroundColor = 0xFF000000,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Composable
+fun TopAppBarPreview() {
+    MaterialTheme(
+        colorScheme = if (isSystemInDarkTheme()) {
+            darkColorScheme()
+        } else {
+            lightColorScheme()
+        }
+    ) {
+        TopAppBar()
+    }
+}
