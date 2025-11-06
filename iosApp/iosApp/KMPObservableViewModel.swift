043bd78149a130f5a7533b98c260220667c40ded
Winston Sung <winston21.sung@gmail.com>
2025-11-06 18:20:32 +0800

    chore: Upgrade dependencies
    * gradle
      * androidx-navigation = "2.9.5" => "2.9.6"
      * composeMultiplatform = "1.9.1" => "1.9.2"
      * kmpObservableViewmodel = "1.0.0-BETA-14" => "1.0.0"
      * ktor = "3.3.1" => "3.3.2"
    * iosApp
      * KMP-ObservableViewModel =  "1.0.0-BETA-14" => "1.0.0"
    
    Bug: #59
    Change-Id: Ia5ceba4e235c8a38c3dbadcf0e9d5f52289f55f1

--- iosApp/iosApp/KMPObservableViewModel.swift
+++ iosApp/iosApp/KMPObservableViewModel.swift
@@ -1,4 +1,4 @@
 import KMPObservableViewModelCore
 import Shared
 
-extension Kmp_observableviewmodel_coreViewModel: ViewModel {}
+extension Kmp_observableviewmodel_coreViewModel: @retroactive ViewModel {}
