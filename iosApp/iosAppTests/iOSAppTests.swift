837d00280732e4c780ce6e65ba685acb75e15eca
Winston Sung <winston21.sung@gmail.com>
2026-02-16 16:06:51 +0800

    chore: Add iosApp xcschemes and tests
    * iosApp.xcscheme
    * iosAppTests
    * iosAppTests.xcscheme
    * iosAppUITests
    * iosAppUITests.xcscheme
    
    Bug: #84
    Change-Id: Ife358cede6820a3042af44c6daf3b40ab75c35d1

---
+++ iosApp/iosAppTests/iOSAppTests.swift
@@ -0,0 +1,8 @@
+import Testing
+
+struct iOSAppTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
